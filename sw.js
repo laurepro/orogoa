3804c9de206f00e44fad6ea6b404cd0b3dba1a15
laurepro <laurent.prott@gmail.com>
2023-07-01 20:23:13 +0200

    sw

--- sw.js
+++ sw.js
@@ -1,13 +1,25 @@
 const CACHE_NAME = `orogoa-v0.1.0`;
 
-// Use the install event to pre-cache all initial resources.
+// Use the install event to pre-cache all initial ressources.
 self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
     cache.addAll([
       '/',
-      '/orogoa.js',
-      '/orogoa.css'
+      '/ressources/orogoa.js',
+      '/ressources/orogoa.css',
+      '/ressources/i18n.js',
+      '/ressources/svg/car1.svg',
+      '/ressources/svg/car2.svg',
+      '/ressources/svg/car3.svg',
+      '/ressources/svg/car4.svg',
+      '/ressources/svg/car5.svg',
+      '/ressources/svg/car6.svg',
+      '/ressources/svg/de.svg',
+      '/ressources/svg/es.svg',
+      '/ressources/svg/fr.svg',
+      '/ressources/svg/gb.svg',
+      '/ressources/svg/arrow.svg'
     ]);
   })());
 });
